a664dc23b931e7b69f28e4f8c71eefa283a17e44
Maloulou99 <113166624+Maloulou99@users.noreply.github.com>
2023-11-21 15:13:34 +0100

    Exeption class added

--- src/main/java/com/example/kohbergbackend/exception/RestExceptionHandler.java
+++ src/main/java/com/example/kohbergbackend/exception/RestExceptionHandler.java
@@ -1,4 +1,30 @@
 package com.example.kohbergbackend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class RestExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleStudentNotFoundException(NotFoundException ex){
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now());
+        responseBody.put("message", ex.getMessage());
+        return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGeneralException(Exception ex){
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now());
+        responseBody.put("message", "An error has occured: " + ex.getMessage());
+        return new ResponseEntity<>(responseBody, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }

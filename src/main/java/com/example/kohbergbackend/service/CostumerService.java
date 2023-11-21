ff3858a85976acf4b2619a410db9a21937071664
Maloulou99 <113166624+Maloulou99@users.noreply.github.com>
2023-11-21 15:27:44 +0100

    Crud made in service class

--- src/main/java/com/example/kohbergbackend/service/CostumerService.java
+++ src/main/java/com/example/kohbergbackend/service/CostumerService.java
@@ -1,8 +1,45 @@
 package com.example.kohbergbackend.service;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.kohbergbackend.model.Costumer;
+import com.example.kohbergbackend.repository.CostumerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CostumerService {
+
+    private CostumerRepository costumerRepository;
+
+    @Autowired
+    public CostumerService(CostumerRepository costumerRepository){
+        this.costumerRepository = costumerRepository;
+    }
+
+    public Costumer createCostumer(Costumer costumer) {
+        return costumerRepository.save(costumer);
+    }
+
+    public List<Costumer> getAllCostumers() {
+        return costumerRepository.findAll();
+    }
+
+    public Optional<Costumer> getCostumerById(int costumerId) {
+        return costumerRepository.findById(costumerId);
+    }
+
+    public Costumer updateCostumer(int costumerId) {
+        if (costumerRepository.existsById(costumerId)) {
+            Costumer updatedCostumer = costumerRepository.setId(costumerId);
+            return costumerRepository.save(updatedCostumer);
+        }
+        return null;
+    }
+
+    public void deleteCostumer(int costumerId) {
+        costumerRepository.deleteById(costumerId);
+    }
+
 }

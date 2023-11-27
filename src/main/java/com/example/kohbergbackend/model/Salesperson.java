5768f0cb77cc141343f9888733ae78fb1e439c56
Maloulou99 <113166624+Maloulou99@users.noreply.github.com>
2023-11-27 11:12:24 +0100

    Transient metode for formatering er tilføjet i modelklasser med LocalDate

--- src/main/java/com/example/kohbergbackend/model/Salesperson.java
+++ src/main/java/com/example/kohbergbackend/model/Salesperson.java
@@ -1,12 +1,11 @@
 package com.example.kohbergbackend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @Entity
 @Getter
@@ -31,6 +30,14 @@ public class Salesperson {
         this.birthday = birthday;
         this.customers = customers;
         this.email = email;
+    }
+
+    @Transient
+    public String getFormattedReminderDate() {
+        // Vælg det ønskede format (fx. "dd.MM.yyyy")
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy", new Locale("da", "DK"));
+        // Formatér LocalDate til en streng
+        return birthday.format(formatter);
     }
 
 }

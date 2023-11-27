f7d40c5e36d90374ba15eafcb99b2498be1490d3
Samim <sami0880@stud.kea.dk>
2023-11-27 12:14:41 +0100

    updated application test

--- src/test/java/com/example/kohbergbackend/KohbergBackendApplicationTests.java
+++ src/test/java/com/example/kohbergbackend/KohbergBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.kohbergbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = KohbergBackendApplication.class)
 class KohbergBackendApplicationTests {
 
     @Test

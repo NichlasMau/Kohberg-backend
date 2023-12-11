f66cc2d2f557a63e2be0ed3ce2a73eb508de16ea
Maloulou99 <113166624+Maloulou99@users.noreply.github.com>
2023-12-11 13:15:13 +0100

    Salesperson er tilføjet i initdata

--- src/main/java/com/example/kohbergbackend/config/InitData.java
+++ src/main/java/com/example/kohbergbackend/config/InitData.java
@@ -1,37 +1,42 @@
 package com.example.kohbergbackend.config;
 
-import com.example.kohbergbackend.dto.CustomerDTO;
-import com.example.kohbergbackend.dto.SaleConverter;
-import com.example.kohbergbackend.dto.SaleDTO;
+import com.example.kohbergbackend.dto.*;
 import com.example.kohbergbackend.model.Customer;
 import com.example.kohbergbackend.model.Sale;
+import com.example.kohbergbackend.model.Salesperson;
 import com.example.kohbergbackend.model.User;
 import com.example.kohbergbackend.repository.UserRepository;
 import com.example.kohbergbackend.service.CustomerService;
 import com.example.kohbergbackend.service.SaleService;
+import com.example.kohbergbackend.service.SalespersonService;
 import com.example.kohbergbackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Component
 public class InitData implements CommandLineRunner {
 
     private final CustomerService customerService;
-    private final UserRepository userRepository;
     private final SaleService saleService;
     private final SaleConverter saleConverter;
+    private final SalespersonService salespersonService;
+    private final CustomerConverter customerConverter;
+
 
 
     @Autowired
-    public InitData(CustomerService customerService, UserRepository userRepository,
-                    SaleService saleService, SaleConverter saleConverter) {
+    public InitData(CustomerService customerService,
+                    SaleService saleService, SaleConverter saleConverter,
+                    SalespersonService salespersonService, CustomerConverter customerConverter) {
         this.customerService = customerService;
-        this.userRepository = userRepository;
         this.saleService = saleService;
         this.saleConverter = saleConverter;
+        this.salespersonService = salespersonService;
+        this.customerConverter = customerConverter;
     }
 
     @Override
@@ -62,5 +67,23 @@ public class InitData implements CommandLineRunner {
 
         // Du kan gøre noget med det oprettede salg, hvis det er nødvendigt
         System.out.println("Oprettet salg med ID: " + createdSale.getSaleID());
+
+        Customer customer = customerConverter.toEntity(newUser);
+
+        // Opret en ny SalespersonDTO
+        SalespersonDTO newSalespersonDTO = new SalespersonDTO(
+                1,
+                "Jane Doe",
+                LocalDate.parse("1995-05-15"),
+                List.of(customer),
+                "jane.doe@example.com"
+        );
+
+
+        // Gem Salesperson ved hjælp af SalespersonService
+        SalespersonDTO createdSalesperson = salespersonService.createSalesperson(newSalespersonDTO);
+
+        // Du kan gøre noget med den oprettede Salesperson, hvis det er nødvendigt
+        System.out.println("Oprettet Salesperson med ID: " + createdSalesperson.id());
     }
 }

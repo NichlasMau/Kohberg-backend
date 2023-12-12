c74793b0008e93f99e4d5e8222b748c0d075c8fe
amirah2700 <amir2700@hotmail.com>
2023-12-12 14:51:18 +0100

    product fixed

--- src/main/java/com/example/kohbergbackend/controller/ProductController.java
+++ src/main/java/com/example/kohbergbackend/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.kohbergbackend.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
